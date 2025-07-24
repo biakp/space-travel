da78f497ccc0671312c85b3298659c70d2c21318
biakp <beatriz.knabbenp@gmail.com>
2025-07-24 16:59:56 -0300

    feat: add routing and authentication pages for login and signup

--- frontend/space-travel/src/App.tsx
+++ frontend/space-travel/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Auth/Login";
+import { SignUp } from "./pages/Auth/SignUp";
+import { Home } from "./pages/Home/Home";
+
 export function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">Space Travel Hub</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
